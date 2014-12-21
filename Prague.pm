32e72fa8e06281102c24100eeaaf9b2783bbee98
Michal Špaček <tupinek@gmail.com>
2014-12-21 17:34:40 +0100

    Add EXAMPLE3 section to doc.

--- Prague.pm
+++ Prague.pm
@@ -134,6 +134,42 @@ For more information about Prague Map, click L<here|https://en.wikipedia.org/wik
  # Output like:
  # XML file: .*/prague-map.xml
 
+=head1 EXAMPLE3
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Map::Tube::GraphViz;
+ use Map::Tube::GraphViz::Utils qw(node_color_without_label);
+ use Map::Tube::Prague;
+
+ # Object.
+ my $obj = Map::Tube::Prague->new;
+
+ # GraphViz object.
+ my $g = Map::Tube::GraphViz->new(
+         'callback_node' => \&node_color_without_label,
+         'driver' => 'neato',
+         'tube' => $obj,
+ ); 
+
+ # Get graph to file.
+ $g->graph('Prague.png');
+
+ # Print file.
+ system "ls -l Prague.png";
+
+ # Output like:
+ # -rw-r--r-- 1 skim skim 292500 Dec 21 17:25 Prague.png
+
+=begin html
+
+<img src="images/Prague.png" alt="Pražské metro" width="300px" height="300px" />
+
+=end html
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
---
+++ examples/ex3.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Map::Tube::GraphViz;
+ use Map::Tube::GraphViz::Utils qw(node_color_without_label);
+ use Map::Tube::Prague;
+
+ # Object.
+ my $obj = Map::Tube::Prague->new;
+
+ # GraphViz object.
+ my $g = Map::Tube::GraphViz->new(
+         'callback_node' => \&node_color_without_label,
+         'driver' => 'neato',
+         'tube' => $obj,
+ ); 
+
+ # Get graph to file.
+ $g->graph('Prague.png');
+
+ # Print file.
+ system "ls -l Prague.png";
+
+ # Output like:
+ # -rw-r--r-- 1 skim skim 292500 Dec 21 17:25 Prague.png
+
+=begin html
+
+<img src="images/Prague.png" alt="Pražské metro" width="300px" height="300px" />
+
+=end html
