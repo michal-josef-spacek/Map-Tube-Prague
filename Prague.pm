68a6a0a65b33d859e227253c6776659b01fbfa90
Michal Špaček <tupinek@gmail.com>
2015-11-03 23:26:57 +0100

    Add EXAMPLE5 section to doc.

--- Prague.pm
+++ Prague.pm
@@ -218,6 +218,57 @@ For more information about Prague Map, click L<here|https://en.wikipedia.org/wik
  # Linka B
  # Linka C
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Prague;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = $ARGV[0];
+
+ # Object.
+ my $obj = Map::Tube::Prague->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Linka A' argument.
+ # Nemocnice Motol
+ # Petřiny
+ # Nádraží Veleslavín
+ # Bořislavka
+ # Dejvická
+ # Hradčanská
+ # Malostranská
+ # Staroměstská
+ # Můstek
+ # Muzeum
+ # Náměstí Míru
+ # Jiřího z Poděbrad
+ # Flora
+ # Želivského
+ # Strašnická
+ # Skalka
+ # Depo Hostivař
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
